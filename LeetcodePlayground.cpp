593c421ae56dfca1e49f473a3f82f1e1434734c1
51VR\yejing <wangyejing@51hitech.com>
2020-05-06 11:35:43 +0800

    Add CMakeList

---
+++ LeetcodePlayground.cpp
@@ -0,0 +1,21 @@
+//
+
+#include "Problems/1_Two_Sum.h"	// change/add headers correspoding to different problems
+#include <iostream>
+
+int main()
+{
+	Solution s;
+
+	// change the input according to the problem
+	std::vector<int> input{ 3, 2, 4 };
+	int target = 6;
+
+	auto res = s.twoSum(input, target);
+
+	for (size_t i = 0; i < res.size(); ++i) {
+		std::cout << res[i] << std::endl;
+	}
+	
+	return 0;
+}
