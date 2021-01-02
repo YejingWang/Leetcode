3a7e9674d3d75d637bd68fc2d3ea78a1e398e3f2
Yejing <yw687@cornell.edu>
2021-01-02 22:49:05 +0800

    961, 1704

---
+++ Problems/1704_Determine_if_String_Halves_Are_Alike.h
@@ -0,0 +1,107 @@
+#pragma once
+
+/* 
+961. N-Repeated Element in Size 2N Array
+https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+
+In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+
+Return the element repeated N times.
+
+
+
+Example 1:
+
+Input: [1,2,3,3]
+Output: 3
+Example 2:
+
+Input: [2,1,2,5,3,2]
+Output: 2
+Example 3:
+
+Input: [5,1,5,2,5,3,5,4]
+Output: 5
+
+
+Note:
+
+4 <= A.length <= 10000
+0 <= A[i] < 10000
+A.length is even
+*/
+
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+class Solution {
+public:
+    int repeatedNTimes(std::vector<int>& A) {
+        // 1. Comparison with unordered_map
+        // Time complexity:     O(N)
+        // Space complexity:    O(N)
+        /*std::unordered_map<int, int> mp;
+        for (size_t i = 0; i < A.size(); ++i) {
+            ++mp[A[i]];
+            if (mp[A[i]] > 1) {
+                return A[i];
+            }
+        }
+        return 0;*/
+
+        // 2. Comparison
+        // Time complexity:     O(N)
+        // Space complexity:    O(N)
+        bool cnt[10000]{ false };
+        for (size_t i = 0; i < A.size(); ++i) {
+            if (cnt[A[i]]) {
+                return A[i];
+            }
+            cnt[A[i]] = true;
+        }
+        return 0;
+
+        // 3. Sort
+        // Time complexity:     O(NlogN)
+        // Space complexity:    O(1)
+        /*size_t half = A.size() / 2;
+        std::sort(A.begin(), A.end());
+        if (A[half - 1] == A[half]) {
+            return A[half];
+        }
+        else {
+            if (A[0] == A[half - 1]) {
+                return A[0];
+            }
+            else {
+                return A[half];
+            }
+        }
+        return 0;*/
+
+        // 4. Classification
+        // Time complexity:     O(N)
+        // Space complexity:    O(1)
+        /*for (size_t i = 2; i < A.size(); ++i) {
+            if (A[i] == A[i - 1] || A[i] == A[i - 2]) {
+                return A[i];
+            }
+        }
+        return A[0];*/
+
+        // 5. Randomization
+        // Time complexity:     O(1) (Q(4) armortized; O(inf) worst case)
+        // Space complexity:    O(1)
+        /*int i = 0;
+        int j = 0;
+        size_t sz = A.size();
+        while (i == j || A[i] != A[j])
+            i = rand() % sz, j = rand() % sz;
+        return A[i];*/
+    }
+};
+
+/*
+Tips:
+*/
---
+++ Problems/961_N-Repeated_Element_in_Size_2N_Array.h
@@ -0,0 +1,105 @@
+#pragma once
+
+/* 
+1. Two Sum
+https://leetcode.com/problems/two-sum/
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        // Solution 2 performs the best both in time complexity and space complexity...
+        // 4 comes closely after it; 3 is moderately worse
+        std::vector<int> res;
+
+        // 1.Brute force
+        // Time complexity:     O(N^2)
+        // Space complexity:    O(1)
+        /*for (size_t i = 0; i < nums.size() - 1; ++i) {
+            for (size_t j = i + 1; j < nums.size(); ++j) {
+                if (nums[i] + nums[j] == target) {
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
+                }
+            }
+        }
+        return res*/
+
+        // 2.Sort + 2 pointers
+        // Time complexity:     O(NlogN)
+        // Space complexity:    O(N)
+        /*std::vector<int> tmp(nums.begin(), nums.end());
+        std::sort(tmp.begin(), tmp.end());
+        int pre = 0, post = nums.size() - 1;
+        while (pre < post) {
+            if (tmp[pre] + tmp[post] < target) {
+                ++pre;
+            }
+            else if (tmp[pre] + tmp[post] > target) {
+                --post;
+            }
+            else {
+                break;
+            }
+        }
+        for (size_t i = 0; i < nums.size(); ++i) {
+            if (nums[i] == tmp[pre]) {
+                res.push_back(i);
+            }
+            else if (nums[i] == tmp[post]) {
+                res.push_back(i);
+            }
+        }
+        return res;*/
+
+        // 3.Uses map (two-pass)
+        // Time complexity:     O(N)
+        // Space complexity:    O(N)
+        /*std::map<int, int> mp;
+        for (size_t i = 0; i < nums.size(); ++i) {
+            mp[target - nums[i]] = i;
+        }
+
+        for (size_t i = 0; i < nums.size(); ++i) {
+            if (mp.find(nums[i]) != mp.end() && mp[nums[i]] != i) {
+                res.push_back(i);
+                res.push_back(mp[nums[i]]);
+                break;
+            }
+        }
+        return res;*/
+
+        // 4.Better map usage (one-pass)
+        // Time complexity:     O(N)
+        // Space complexity:    O(N)
+        std::unordered_map<int, int> ump;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (ump.find(target - nums[i]) != ump.end()) {
+                return std::vector<int>{i, ump[target - nums[i]]};
+            }
+            ump[nums[i]] = i;
+        }
+        return std::vector<int>{};
+    }
+};
+
+/*
+Tips:
+*/
