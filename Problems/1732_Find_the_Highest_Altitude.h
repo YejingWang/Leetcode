4774dcc6e490e4bb3d44498a7f1c955ca3f6f915
Yejing <yw687@cornell.edu>
2021-02-28 23:57:53 +0800

    1732

---
+++ Problems/1732_Find_the_Highest_Altitude.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+/* 
+1732. Find the Highest Altitude
+https://leetcode.com/problems/find-the-highest-altitude/
+
+There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+
+
+Example 1:
+
+Input: gain = [-5,1,5,0,-7]
+Output: 1
+Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+Example 2:
+
+Input: gain = [-4,-3,-2,-1,4,3,2]
+Output: 0
+Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+
+
+Constraints:
+
+n == gain.length
+1 <= n <= 100
+-100 <= gain[i] <= 100
+*/
+
+#include <vector>
+
+class Solution {
+public:
+    int largestAltitude(std::vector<int>& gain) {
+        // 1. Traversal
+        // Time complexity:     O(N)
+        // Space complexity:    O(1)
+        int res = 0;
+        int altitude = 0;
+        for (auto& g : gain) {
+            altitude += g;
+            res = altitude > res ? altitude : res;
+        }
+        return res;
+    }
+};
+
+/*
+Tips:
+*/
