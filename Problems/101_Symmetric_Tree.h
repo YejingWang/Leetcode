de35273dbce31329a9b824b3d90f02716224897c
Yejing <yw687@cornell.edu>
2021-07-29 23:46:11 +0800

    102

--- Problems/101_Symmetric_Tree.h
+++ Problems/101_Symmetric_Tree.h
@@ -1,6 +1,6 @@
 #pragma once
 
-/* 
+/*
 101. Symmetric Tree
 https://leetcode.com/problems/symmetric-tree/
 
@@ -24,7 +24,7 @@ Output: true
 
 Example 2:
       1
-	 / \
+     / \
     /   \
    2     2
     \     \
@@ -47,64 +47,64 @@ Follow up: Could you solve it both recursively and iteratively?
 
 // Definition for a binary tree node.
 struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode() : val(0), left(nullptr), right(nullptr) {}
-	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Solution {
 public:
-	bool isTwoTreeSymmetric(TreeNode* r1, TreeNode* r2) {
-		if (!r1 && !r2) return true;
-		if (!r1 || !r2) return false;
-		if (r1->val != r2->val) return false;
-		return isTwoTreeSymmetric(r1->left, r2->right) && isTwoTreeSymmetric(r1->right, r2->left);
-	}
+    bool isTwoTreeSymmetric(TreeNode* r1, TreeNode* r2) {
+        if (!r1 && !r2) return true;
+        if (!r1 || !r2) return false;
+        if (r1->val != r2->val) return false;
+        return isTwoTreeSymmetric(r1->left, r2->right) && isTwoTreeSymmetric(r1->right, r2->left);
+    }
 
-	bool isSymmetric(TreeNode* root) {
-		// 1. Recursive Approach
-		// Time complexity:     O(N)
-		// Space complexity:    O(lgN), worst cast O(N)
-		if (!root) return true;
-		return isTwoTreeSymmetric(root->left, root->right);
+    bool isSymmetric(TreeNode* root) {
+        // 1. Recursive Approach
+        // Time complexity:     O(N)
+        // Space complexity:    O(lgN), worst cast O(N)
+        if (!root) return true;
+        return isTwoTreeSymmetric(root->left, root->right);
 
-		// 2. Iterative Approach
-		// Time complexity:     O(N)
-		// Space complexity:    O(lgN), worst cast O(N)
-		/*if (!root) return true;
-		std::queue<TreeNode*> q1;
-		std::queue<TreeNode*> q2;
-		q1.push(root->left);
-		q2.push(root->right);
-		while (!q1.empty() && !q2.empty()) {
-			auto t1 = q1.front();
-			auto t2 = q2.front();
-			q1.pop();
-			q2.pop();
-			if (!t1) {
-				if (t2) {
-					return false;
-				}
-			}
-			else {
-				if (!t2) {
-					return false;
-				}
-				if (t1->val != t2->val) {
-					return false;
-				}
-				q1.push(t1->left);
-				q1.push(t1->right);
-				q2.push(t2->right);
-				q2.push(t2->left);
-			}
+        // 2. Iterative Approach
+        // Time complexity:     O(N)
+        // Space complexity:    O(lgN), worst cast O(N)
+        /*if (!root) return true;
+        std::queue<TreeNode*> q1;
+        std::queue<TreeNode*> q2;
+        q1.push(root->left);
+        q2.push(root->right);
+        while (!q1.empty() && !q2.empty()) {
+            auto t1 = q1.front();
+            auto t2 = q2.front();
+            q1.pop();
+            q2.pop();
+            if (!t1) {
+                if (t2) {
+                    return false;
+                }
+            }
+            else {
+                if (!t2) {
+                    return false;
+                }
+                if (t1->val != t2->val) {
+                    return false;
+                }
+                q1.push(t1->left);
+                q1.push(t1->right);
+                q2.push(t2->right);
+                q2.push(t2->left);
+            }
 
-		}
-		return true;*/
-	}
+        }
+        return true;*/
+    }
 };
 
 /*
---
+++ Problems/102_Binary_Tree_Level_Order_Traversal.h
@@ -0,0 +1,145 @@
+#pragma once
+
+/* 
+102. Binary Tree Level Order Traversal
+https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+
+
+Example 1:
+   3
+  / \
+ /   \
+9     20
+      / \
+     /   \
+    15    7
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+
+Example 3:
+
+Input: root = []
+Output: []
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+*/
+
+#include <vector>
+#include <queue>
+#include <utility>
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void levelorder(TreeNode* root, int l, std::vector<std::vector<int>>& v) {
+        if (v.size() < l) {
+            std::vector<int> vt{ root->val };
+            v.push_back(vt);
+        }
+        else {
+            v[l - 1].push_back(root->val);
+        }
+        if (root->left) {
+            levelorder(root->left, l + 1, v);
+        }
+        if (root->right) {
+            levelorder(root->right, l + 1, v);
+        }
+    }
+
+    void level(std::queue<TreeNode*>& p, std::queue<TreeNode*>& q, std::vector<std::vector<int>>& v) {
+        std::vector<int> vt;
+        while (!p.empty()) {
+            auto tmp = p.front();
+            p.pop();
+            vt.push_back(tmp->val);
+            if (tmp->left) {
+                q.push(tmp->left);
+            }
+            if (tmp->right) {
+                q.push(tmp->right);
+            }
+        }
+        if (vt.size()) {
+            v.push_back(vt);
+        }
+    }
+
+    std::vector<std::vector<int>> levelOrder(TreeNode* root) {
+        // 1. Iteration (BFS)
+        // Time complexity:     O(N)
+        // Space complexity:    O(N)
+        /*std::vector<std::vector<int>> v;
+        if (!root) return v;
+        std::queue<std::pair<int, TreeNode*>> q;
+        q.push(std::make_pair(1, root));
+        while (!q.empty()) {
+            auto tmp = q.front();
+            q.pop();
+            if (v.size() < tmp.first) {
+                std::vector<int> vt{ tmp.second->val };
+                v.push_back(vt);
+            }
+            else {
+                v[tmp.first - 1].push_back(tmp.second->val);
+            }
+            if (tmp.second->left) {
+                q.push(std::make_pair(tmp.first + 1, tmp.second->left));
+            }
+            if (tmp.second->right) {
+                q.push(std::make_pair(tmp.first + 1, tmp.second->right));
+            }
+        }
+        return v;*/
+
+        // 2. Recursion (DFS)
+        // Time complexity:     O(N)
+        // Space complexity:    O(lgN), worst case O(N)
+        /*std::vector<std::vector<int>> v;
+        if (!root) return v;
+        levelorder(root, 1, v);
+        return v;*/
+
+        // 3. Iteration with two queues
+        // Time complexity:     O(N)
+        // Space complexity:    O(N)
+        std::vector<std::vector<int>> v;
+        if (!root) return v;
+        std::queue<TreeNode*> p, q;
+        p.push(root);
+        while (!p.empty() || !q.empty()) {
+            if (!p.empty()) {
+                level(p, q, v);
+            }
+            else {
+                level(q, p, v);
+            }
+        }
+        return v;
+    }
+};
+
+/*
+Tips:
+*/
